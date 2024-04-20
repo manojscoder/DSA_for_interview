7dfdc6a06b254d75ac469aa0fc7b41f054b5d96e
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-20 20:21:35 +0530

    Create Two_sum_2_input_array_is_sorted.py

---
+++ Two_pointers/Python/Two_sum_2_input_array_is_sorted.py
@@ -0,0 +1,14 @@
+# Time: O(n)
+# Space: O(1)
+# Problem link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
