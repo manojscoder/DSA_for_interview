62edc353115e9e5f5e68920caaf6636ddefb94ff
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-20 19:53:19 +0530

    Create Top_K_frequent_element.py

---
+++ Array/Python/Top_K_frequent_element.py
@@ -0,0 +1,20 @@
+# Time: O(n)
+# Space: O(n)
+# Problem link: https://leetcode.com/problems/top-k-frequent-elements/description/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
