2f930fbf89c41fe00f04a68d7eb9d94cb7e8b7ad
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-20 19:43:01 +0530

    Create Valid_Anagram.py

---
+++ Array/Python/Valid_Anagram.py
@@ -0,0 +1,13 @@
+# Time: O(n)
+# Space: O(n)
+# Problem link: https://leetcode.com/problems/valid-anagram/description/
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
