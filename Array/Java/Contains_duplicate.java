b3d46e994692747d121e56c2dfdf41f340bbe84d
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-20 19:54:41 +0530

    Create Contains_duplicate.java

---
+++ Array/Java/Contains_duplicate.java
@@ -0,0 +1,15 @@
+// Time: O(n)
+// Space: O(n)
+// Problem link: https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums){
+            if(!set.add(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
