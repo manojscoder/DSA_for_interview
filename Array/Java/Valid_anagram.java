bcfd3acddc9ed1c66957755f1eb742e07615e1be
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-20 19:58:53 +0530

    Create Valid_anagram.java

---
+++ Array/Java/Valid_anagram.java
@@ -0,0 +1,26 @@
+// Time: O(n)
+// Space: O(n)
+// Problem link: https://leetcode.com/problems/valid-anagram/description/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] store = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
+        }
+
+        for(int n : store){
+            if(n != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
