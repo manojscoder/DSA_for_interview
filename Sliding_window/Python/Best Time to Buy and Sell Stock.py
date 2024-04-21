0010da17611358788d4a5d304395e6463a9e4c06
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-21 15:11:15 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Sliding_window/Python/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,13 @@
+# Time: O(n)
+# Space: O(1)
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            res = max(res, price - lowest)
+        return res
