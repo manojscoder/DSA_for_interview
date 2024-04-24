b6ad90c6cfc6ec64c7c606c76b3ca20f82078b48
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-04-24 21:03:47 +0530

    Create Daily Temperatures.py

---
+++ Stack/Python/Daily Temperatures.py
@@ -0,0 +1,14 @@
+# Time: O(n)
+# Space: O(n)
+# Problem link: https://leetcode.com/problems/daily-temperatures/description/
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
