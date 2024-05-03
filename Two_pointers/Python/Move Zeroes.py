ef4cbb768c944ce0d7cdcb365b1921119b549c88
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-05-03 19:09:21 +0530

    Create Move Zeroes.py

---
+++ Two_pointers/Python/Move Zeroes.py
@@ -0,0 +1,16 @@
+# Time: O(n)
+# Space: O(1)
+# Problem link: https://leetcode.com/problems/move-zeroes/description/
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        for fast in range(len(nums)):
+            
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            if nums[slow] != 0:
+                slow += 1
