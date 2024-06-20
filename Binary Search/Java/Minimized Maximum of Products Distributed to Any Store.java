773dc238b2caa3b59fe7599cd3f539a0b14e582d
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-06-20 13:26:49 +0530

    Create Minimized Maximum of Products Distributed to Any Store.java

---
+++ Binary Search/Java/Minimized Maximum of Products Distributed to Any Store.java
@@ -0,0 +1,34 @@
+// Problem link: https://leetcode.com/problems/minimized-maximum-of-products-distributed-to-any-store/description/
+// Time: O(n log max(quantities))
+// Space: O(1)
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int left = 1, right = 0, result = -1, mid;
+
+        for(int i : quantities)
+            right = Math.max(right, i);
+
+        while(left <= right) {
+            mid = (left + right) / 2;
+
+            if(isPossible(quantities, n, mid)) {
+                result = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isPossible(int[] quantities, int n, int diff) {
+        int total = 0;
+
+        for(int q : quantities)
+            total += Math.ceil((double) q / diff);
+
+        return total <= n;
+    }
+}
