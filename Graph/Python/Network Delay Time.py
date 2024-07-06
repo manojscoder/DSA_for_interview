b3793b23e24514fff51b7a13bf86cadc448aaf3e
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-06 21:58:07 +0530

    Update Network Delay Time.py

--- Graph/Python/Network Delay Time.py
+++ Graph/Python/Network Delay Time.py
@@ -1,4 +1,6 @@
 # Problem link: https://leetcode.com/problems/network-delay-time/description/
+
+# Approach 1: Dijkstras
 # Time: O(E log V)
 # Space: O(V + E)
 class Solution:
@@ -28,6 +30,29 @@ class Solution:
         
         for i in range(1, len(dist)):
             if math.isinf(dist[i]):
+                return -1
+            result = max(result, dist[i])
+
+        return result
+
+
+# Approach 2: Bellman ford
+# Time: O(E * V)
+# Space: O(V)
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        dist = [float('inf') for _ in range(n)]
+        dist[k - 1], result = 0, -1
+
+        for i in range(n - 1):
+            for j in range(len(times)):
+                u, v, t = times[j]
+
+                if dist[u - 1] != float('inf') and dist[u - 1] + t < dist[v - 1]:
+                    dist[v - 1] = dist[u - 1] + t
+
+        for i in range(n):
+            if dist[i] == float('inf'):
                 return -1
             result = max(result, dist[i])
 
