33fd897a5e1979bdac1b259005c7cae512c6c3d7
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-07 22:37:05 +0530

    Create Binary Serach.py

---
+++ Binary Search/Python/Binary Serach.py
@@ -0,0 +1,17 @@
+# Problem link: https://leetcode.com/problems/binary-search/description/
+# Time: O(log n)
+# Space: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return -1
