1c748b46383caca96637276b0c2c0535539a7407
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-09 20:45:33 +0530

    Create Bubble_sort.c

---
+++ Sorting_algorithms/C/Bubble_sort.c
@@ -0,0 +1,23 @@
+// Time:
+// Best: O(n)
+// Average: O(n ^ 2)
+// Worst: O(n ^ 2)
+void bubbleSort(int* arr, int n) {
+    
+    for(int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+        
+        for(int j = 0; j < n - 1 - i; j++) {
+            
+            if(arr[j + 1] < arr[j]) {
+                swapped = 1;
+                arr[j + 1] += arr[j];
+                arr[j] = arr[j + 1] - arr[j];
+                arr[j + 1] -= arr[j];
+            }
+        }
+        
+        if(!swapped)
+            break;
+    }
+}
