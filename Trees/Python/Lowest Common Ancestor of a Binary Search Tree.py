04c510586d5342e669ab47ba59bed82ad92b7fe9
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-17 16:52:06 +0530

    Create Lowest Common Ancestor of a Binary Search Tree.py

---
+++ Trees/Python/Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,12 @@
+# Problem link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+# Time: O(log n)
+# Space: O(1)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
