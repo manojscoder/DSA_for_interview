53b55656a1c65abd1970682bfe34354cfb24c83e
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-18 13:52:18 +0530

    Update Insertion_sort.java

--- Sorting_algorithms/Java/Insertion_sort.java
+++ Sorting_algorithms/Java/Insertion_sort.java
@@ -5,6 +5,7 @@
 
 // Space: O(1)
 
+//Approach: 1
 import java.util.Arrays;
 public class Main
 {
@@ -27,4 +28,32 @@ public class Main
 	        }
 	    }
 	}
+}
+
+
+
+// Approach: 2
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {89, 45, 68, 90, 29, 34, 17};
+        insertionSort(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    private static void insertionSort(int[] arr, int len) {
+        int val, j;
+        
+        for(int idx = 1; idx < len; idx++) {
+            val = arr[idx];
+            j = idx - 1;
+            
+            while(j >= 0 && arr[j] > val) {
+                arr[j + 1] = arr[j--];
+            }
+            
+            arr[j + 1] = val;
+        }
+    }
 }
