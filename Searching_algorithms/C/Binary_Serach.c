833f2de782c1edb3609642be9205a8dda0367151
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-19 09:43:24 +0530

    Create Binary_Serach.c

---
+++ Searching_algorithms/C/Binary_Serach.c
@@ -0,0 +1,28 @@
+// Time:
+// Best case: O(1)
+// Average case: O(log n)
+// Worst case: O(log n)
+
+// Space: O(1)
+
+int binarySearch(int* arr, int key, int len) {
+
+  int left = 0, right = len - 1, mid;
+
+  while(left <= right) {
+    
+    mid = (left + right) / 2;
+    
+    if(arr[mid] == key) {
+      return mid;
+    }
+    else if(arr[mid] > key) {
+      right = mid - 1;
+    }
+    else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+}
