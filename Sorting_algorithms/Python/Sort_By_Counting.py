2874ef1d4ed3100b762edd0c5cadddebb4edd29e
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-21 00:21:47 +0530

    Update Sort_By_Counting.py

--- Sorting_algorithms/Python/Sort_By_Counting.py
+++ Sorting_algorithms/Python/Sort_By_Counting.py
@@ -1,6 +1,6 @@
 # Time: O(n ^ 2)
 # Space: O(n)
-
+# Without duplicates
 def sortByCount(arr, n):
     count = [0] * n
     result = [0] * n
@@ -14,5 +14,26 @@ def sortByCount(arr, n):
                 
     for i in range(n):
         result[count[i]] = arr[i]
+    
+    return result
+
+# Time: O(n + k)
+# Space: O(n + k)
+# With duplicates
+def sortByCount(arr, size):
+    low, up = min(arr), max(arr)
+    
+    count = [0] * (up - low + 1)
+    result = [0] * size
+    
+    for num in arr:
+        count[num - low] += 1
+    
+    for idx in range(1, len(count)):
+        count[idx] += count[idx - 1]
+    
+    for num in arr:
+        result[count[num - low] - 1] = num
+        count[num - low] -= 1
     
     return result
