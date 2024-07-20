aebc8f7befc39e5cea36fd850d32caf3e80f114a
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-21 00:16:50 +0530

    Update Sort_By_Counting.c

--- Sorting_algorithms/C/Sort_By_Counting.c
+++ Sorting_algorithms/C/Sort_By_Counting.c
@@ -1,7 +1,6 @@
 // Time: O(n ^ 2)
 // Space: O(n)
-
-// Without repeatation
+// Without duplicates
 int* sortByCount(int* arr, int len) {
     int count[len];
     int* result = (int*)malloc(len * sizeof(int));
@@ -25,6 +24,45 @@ int* sortByCount(int* arr, int len) {
         result[count[i]] = arr[i];
     }
     
+    return result;
+}
+
+// Time: O(n + k)
+// Space: O(n + k)
+// With duplicates
+int* sortByCount(int* arr, int len) {
+    int low = arr[0], up = arr[0];
+    
+    for(int idx = 0; idx < len; idx++) {
+        low = low > arr[idx] ? arr[idx] : low;
+        up = up < arr[idx] ? arr[idx] : up;
+    }
+    
+    int* count = (int*)malloc((up - low + 1) * sizeof(int));
+    int* result = (int*)malloc(len * sizeof(int));
+    
+    
+    for(int idx = 0; idx < up - low + 1; idx++) {
+        count[idx] = 0;
+    }
+    
+    
+    for(int idx = 0; idx < len; idx++) {
+        count[arr[idx] - low] += 1;
+    }
+    
+    
+    for(int idx = 1; idx < up - low + 1; idx++) {
+        count[idx] += count[idx - 1];
+    }
+    
+    
+    for(int idx = 0; idx < len; idx++) {
+        result[count[arr[idx] - low] - 1] = arr[idx];
+        count[arr[idx] - low]--;
+    }
+    
+    free(count);
     return result;
 }
 
