184319662f9b9e91f0aa995945fabcc8d2026d58
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-07-21 00:05:36 +0530

    Update Sort_By_Counting.java

--- Sorting_algorithms/Java/Sort_By_Counting.java
+++ Sorting_algorithms/Java/Sort_By_Counting.java
@@ -1,5 +1,6 @@
 // Time: O(n ^ 2)
 // Space: O(n)
+// Without duplicates
 private static int[] sortByCounting(int[] arr,int len) {
   
         int[] count = new int[len];
@@ -24,3 +25,34 @@ private static int[] sortByCounting(int[] arr,int len) {
         return result;
         
 }
+
+// Time: O(n + k)
+// Space: O(n + k)
+// With duplicates
+private static int[] sortByCount(int[] arr, int len) {
+        int low = Integer.MAX_VALUE, up = Integer.MIN_VALUE;
+        
+        for(int num : arr) {
+            low = Math.min(low, num);
+            up = Math.max(up, num);
+        }
+        
+        int[] count = new int[up - low + 1];
+        int[] result = new int[len];
+        
+        for(int num : arr) {
+            count[num - low]++;
+        }
+        
+        for(int idx = 1; idx < count.length; idx++) {
+            count[idx] += count[idx - 1];
+        }
+        
+        for(int num : arr) {
+            result[count[num - low] - 1] = num;
+            count[num - low]--;
+        }
+        
+        return result;
+  }
+
