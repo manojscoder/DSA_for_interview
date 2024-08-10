b41dcd30b4d4134bc30ac57a6c3bf5545c9a4be0
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-08-10 11:48:39 +0530

    Update Battleships in a Board.java

--- Matrix/Java/Battleships in a Board.java
+++ Matrix/Java/Battleships in a Board.java
@@ -8,27 +8,14 @@ class Solution {
         for(int row = 0; row < rows; row++) {
             for(int col = 0; col < cols; col++) {
                 if(board[row][col] == 'X') {
-                    this.dfs(row, col, board, rows, cols);
+                    if((row > 0 && board[row - 1][col] == 'X') || (col > 0 && board[row][col - 1] == 'X')) {
+                        continue;
+                    }
                     result += 1;
                 }
             }
         }
 
         return result;
-    }
-
-    private void dfs(int row, int col, char[][] board, int rows, int cols) {
-        if(row < 0 || col < 0 || row == rows || col == cols) {
-            return;
-        }
-
-        board[row][col] = '.';
-
-        if(row + 1 < rows && board[row + 1][col] == 'X') {
-            this.dfs(row + 1, col, board, rows, cols);
-        }
-        else if(col + 1 < cols && board[row][col + 1] == 'X') {
-            this.dfs(row, col + 1, board, rows, cols);
-        }
     }
 }
