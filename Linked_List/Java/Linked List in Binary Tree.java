d0e9e1a8fae5aaceb628bd88bf9996661fbc71ed
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-09-07 20:46:46 +0530

    Create Linked List in Binary Tree.java

---
+++ Linked_List/Java/Linked List in Binary Tree.java
@@ -0,0 +1,21 @@
+// Problem link: https://leetcode.com/problems/linked-list-in-binary-tree/description
+// Time: O(n * m)
+// Space: O(n * m)
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(root == null) {
+            return false;
+        }
+        return this.check(head, root) || this.isSubPath(head, root.left) || this.isSubPath(head, root.right);
+    }
+
+    private boolean check(ListNode head, TreeNode root) {
+        if(head == null) {
+            return true;
+        }
+        else if(root == null || head.val != root.val) {
+            return false;
+        }
+        return this.check(head.next, root.left) || this.check(head.next, root.right);
+    }
+}
