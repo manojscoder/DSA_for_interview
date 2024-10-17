f4c93cf58daa0c8feb0c763102074729d5571ea8
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-10-17 06:03:58 +0530

    Create Maximum Swap.java

---
+++ Math and Geometry/Java/Maximum Swap.java
@@ -0,0 +1,39 @@
+// Problem link: https://leetcode.com/problems/maximum-swap/description/
+// Time: O(n)
+// Space: O(n)
+class Solution {
+    public int maximumSwap(int num) {
+        List<Integer> store = new ArrayList<>();
+        int temp = num, result = 0, idx;
+
+        while(temp > 0) {
+            store.add(0, temp % 10);
+            temp /= 10;
+        }
+
+        List<Integer> sortedList = new ArrayList<>(store);
+        sortedList.sort(Collections.reverseOrder());
+
+        for(int i = 0; i < store.size(); i++) {
+            if(sortedList.get(i) != store.get(i)) {
+                idx = store.size() - 1;
+
+                while(store.get(idx) != sortedList.get(i)) {
+                    idx--;
+                }
+                
+                temp = store.get(i);
+                store.set(i, store.get(idx));
+                store.set(idx, temp);
+                break;
+            }
+        }
+
+
+        for(int n : store) {
+            result = result * 10 + n;
+        }
+
+        return result;
+    }
+}
