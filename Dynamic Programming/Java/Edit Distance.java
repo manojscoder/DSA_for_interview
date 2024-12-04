4f3ce55f1b538891073213a4ac51313c88f2dba6
Manoj S <134809278+manojscoder@users.noreply.github.com>
2024-12-04 21:20:42 +0530

    Update Edit Distance.java

--- Dynamic Programming/Java/Edit Distance.java
+++ Dynamic Programming/Java/Edit Distance.java
@@ -1,22 +1,32 @@
 // Problem link: https://leetcode.com/problems/edit-distance/description/
 // Time: O(n * m)
 // Space: O(m)
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        rows, cols = len(word1), len(word2)
-        prev = [val for val in range(cols, -1, -1)]
-        curr = [0] * (cols + 1)
-        
-        for row in range(rows - 1, -1, -1):
-            curr[-1] = rows - row
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int rows = word1.length(), cols = word2.length();
+        int[] prev = new int[cols + 1], curr = new int[cols + 1];
 
-            for col in range(cols - 1, -1, -1):
-                if word1[row] == word2[col]:
-                    curr[col] = prev[col + 1]
-                else:
-                    curr[col] = 1 + min(curr[col + 1], prev[col], prev[col + 1])
-            
-            for index in range(cols + 1):
-                prev[index] = curr[index]
-        
-        return prev[0]
+        for(int index = cols; index >= 0; index--) {
+            prev[cols - index] = index;
+        }
+
+        for(int row = rows - 1; row >= 0 ; row--) {
+            curr[cols] = rows - row;
+
+            for(int col = cols - 1; col >= 0; col--) {
+                if(word1.charAt(row) == word2.charAt(col)) {
+                    curr[col] = prev[col + 1];
+                }
+                else {
+                    curr[col] = 1 + Math.min(curr[col + 1], Math.min(prev[col], prev[col + 1]));
+                }
+            }
+
+            for(int index = 0; index <= cols; index++) {
+                prev[index] = curr[index];
+            }
+        }
+
+        return prev[0];
+    }
+}
